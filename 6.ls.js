c5bd99be95be4aa515b0ca4a08b14c30fce59382
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2023-08-15 12:48:49 -0400

    first application

---
+++ 6.ls.js
@@ -0,0 +1,12 @@
+const fs = require('node:fs')
+
+fs.readdir('.', (err, files) => {
+    if(err) {
+        console.error('Error when tried to read the dir: ', err)
+        return;
+    }
+
+    files.forEach(file => {
+        console.log(file)
+    })
+})
---
+++ mjs/index.mjs
@@ -0,0 +1,4 @@
+
+// import with ES Modules
+import { sum } from './sum.mjs';
+console.log(sum(2,5))
