ce5ed7ec7baceb6cb122edda139a017a8013eb8b
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2023-09-06 18:00:55 -0400

    clase 2 finished

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,24 +1,40 @@
 const express = require('express')
-const app = express()
+const ditto = require('./pokemon/ditto.json')
 
 const PORT = process.env.PORT ?? 3000
+const app = express()
+
+app.disable('x-powered-by')
+
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+//   let body = ''
+
+//   // listen the data event
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.tiemstamp = Date.now()
+//     req.body = data
+//     next()
+//   })
+// })
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Inicio' })
+  res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
+  res.status(201).json(req.body)
+})
 
-  // listen the data event
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    data.tiemstamp = Date.now()
-    res.status(201).json(data)
-  })
+app.use((req, res) => {
+  res.status(404).send('<h1>404</h1>')
 })
 
 app.listen(PORT, () => {
